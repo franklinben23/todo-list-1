8b93d2c631abf072b3b3b90dc83717b69b2eda8d
franklin <franklinbenjamin032@gmail.com>
2022-02-18 16:08:57 -0400

    add list items, create render function

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+const taskList = document.querySelector('.task-list');
+
+const list = [{ name: 'wash the dishes', id: 1, completed: false }, { name: 'walk the dog', id: 2, completed: false }, { name: 'dry the clothes', id: 3, completed: false }];
+
+const render = () => {
+  list.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList.add('task');
+    const label = document.createElement('label');
+    label.classList.add('task-label');
+    label.innerHTML = task.name;
+    li.append(label);
+    taskList.appendChild(li);
+  });
+};
+
+render();
